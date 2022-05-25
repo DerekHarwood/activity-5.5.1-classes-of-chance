5e884452717c960899b32b879435c018f3ad26f1
DerekHarwood <101199163+DerekHarwood@users.noreply.github.com>
2022-05-25 19:37:49 -0400

    Update app.js

--- app.js
+++ app.js
@@ -1,14 +1,29 @@
 class Casino {
-    // Write code here
-};
+    constructor(name, isFakeCoin) {
+        this.name = name;
+        this.timesPlayed = 0;
+        this.isFakeCoin = isFakeCoin;
+    }
+
+    playGame(betAmount) {
+        if (Math.random() <= 0.5 || this.isFakeCoin) {
+            console.log(this.name + "Casino Wins!");
+            this.timesPlayed = 0;
+        } else  {
+            console.log("Player Wins!" + betAmount * (this.timesPlayed + 1) + " dollars from " + this.name + "!");
+            this.timesPlayed += 1;
+        } 
+    }
+}
+
 
 // TESTS
 const myCasino = new Casino("HackerU Casino");
 console.log(myCasino);
 myCasino.playGame(5);
-myCasino.playGame(15);
-myCasino.playGame(25);
-myCasino.playGame(35);
+// myCasino.playGame(15);
+// myCasino.playGame(25);
+// myCasino.playGame(35);
 
 // BONUS TESTS
 /*
